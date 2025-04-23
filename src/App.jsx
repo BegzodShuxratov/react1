fa60101b3d54208299e6c13d4385a8c1327ae0c0
Mirabbos Sobitjonov <sobitjonovmirabbos1@gmail.com>
2025-04-23 19:12:44 +0500

    g

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from './components/Header'
 import Hero from './components/Hero'
 import Customer from './components/Customer'
-import Menu from './components/menu'
+import MenuComponent from './components/MenuComponent'
 
 
 
@@ -11,8 +11,8 @@ export default function App() {
     <div>
       <Header />
       <Hero />
-      <Customer />
-      <Menu />
+      <Customer/>
+      <MenuComponent/>
     </div>
   )
 }
--- src/components/MenuComponent.jsx
+++ src/components/MenuComponent.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Menu.css'
 
-export default function Menu() {
+export default function MenuComponent() {
   return (
     <div className='menu container'>
         <div className="ota">
